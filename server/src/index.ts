0ac7a851d3eb37b55649a6ab1ac99e13b827cd1d
LordAldi <aldianugra09@gmail.com>
2021-10-22 14:36:35 +0700

    add redis to implement cookie session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,61 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { MyContext } from "./types";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
 
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
   const app = express();
+  app.use(cors());
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10years,
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https
+      },
+      saveUninitialized: false,
+      name: "qid",
+      secret: "ksncoanvcsldksncuuusoeddjskklnsdj",
+      resave: false,
+    })
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -38,10 +39,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    //not login
+    if (!req.session.userId) return null;
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length < 2) {
       return {
@@ -83,13 +93,15 @@ export class UserResolver {
       }
     }
 
+    //store user id in session === login in cookie
+    req.session.userId = user.id;
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -113,7 +125,7 @@ export class UserResolver {
         ],
       };
     }
-    await em.persistAndFlush(user);
+    req.session.userId = user.id;
     return { user };
   }
 }
